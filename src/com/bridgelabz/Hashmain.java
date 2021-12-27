d3e6b0eb2ce6412031995ecf5c1c6460a6d58854
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-26 21:32:33 -0800

    Added: Frequency Of Words In Sentence

--- src/com/bridgelabz/Hashmain.java
+++ src/com/bridgelabz/Hashmain.java
@@ -4,6 +4,20 @@ public class Hashmain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Hashtable Program");
+		String sentence = "to be or not to be";
+		HashMap<String,Integer> hashMap = new HashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word: words) {
+			hashMap.get(word);
+			Integer value = hashMap.get(word);
+			if(value == null)
+				value = 1;
+			else
+				value = value + 1;
+			hashMap.add(word,value);
+		}
+		int frequency = hashMap.get("to");
+		System.out.println(hashMap);
 	}
 
 }
---
+++ src/com/bridgelabz/INode.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public interface INode <K extends Comparable> {
+	K getKey();
+	void setkey(K key);
+	
+	INode<K> getNext();
+	
+	void setNext(INode<K> next);
+}
